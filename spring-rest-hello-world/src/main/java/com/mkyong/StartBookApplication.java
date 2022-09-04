b653e44b6d46fe049262e53bd75dcfa9ca5a2cf5
Thimmaiah_S <Thimmaiah_S@4HF22R2-LTOTSP.psb.bls.gov>
2022-09-04 12:36:19 -0400

    include bls

--- spring-rest-hello-world/src/main/java/com/mkyong/StartBookApplication.java
+++ spring-rest-hello-world/src/main/java/com/mkyong/StartBookApplication.java
@@ -21,9 +21,9 @@ public class StartBookApplication {
     @Bean
     CommandLineRunner initDatabase(BookRepository repository) {
         return args -> {
-            repository.save(new Book("A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41")));
-            repository.save(new Book("The Life-Changing Magic of Tidying Up", "Marie Kondo", new BigDecimal("9.69")));
-            repository.save(new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", new BigDecimal("47.99")));
+            repository.save(new Book("BLS - A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41")));
+            repository.save(new Book("BLS - The Life-Changing Magic of Tidying Up", "Marie Kondo", new BigDecimal("9.69")));
+            repository.save(new Book("BLS - Refactoring: Improving the Design of Existing Code", "Martin Fowler", new BigDecimal("47.99")));
         };
     }
 }
